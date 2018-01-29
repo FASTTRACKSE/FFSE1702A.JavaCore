48bf0ebee38b83831232efdca666e2a45dd44922
ThanhCL <thanhcl@gmail.com>
2018-01-29 23:27:55 +0700

    HelloWorld created

---
+++ ThanhCL/Hello World/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		System.out.println("I am student of FFSE1702!");
+		System.out.println("I am very happy to join Sementer II");
+	}
+
+}
