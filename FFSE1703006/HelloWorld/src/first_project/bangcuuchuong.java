94f02f4b95273078f5307711c7945c9b3e0a7774
junbjn98 <23205674+junbjn98@users.noreply.github.com>
2018-01-31 10:17:13 +0700

    FFSE1703006
    so hang + so cot

--- FFSE1703006/HelloWorld/src/first_project/bangcuuchuong.java
+++ FFSE1703006/HelloWorld/src/first_project/bangcuuchuong.java
@@ -5,12 +5,13 @@ public class bangcuuchuong {
 
 	public static void main(String[] args) {
 		Scanner scanIn = new Scanner(System.in);
-		int num;
-		System.out.print("Input number: ");
-		num = scanIn.nextInt();
-		
-		for (int i = 1; i <= 9; i++) {
-			for (int j = 1; j <= num; j++) {
+		int cot, hang;
+		System.out.print("So cot: ");
+		cot = scanIn.nextInt();
+		System.out.print("So hang: ");
+		hang = scanIn.nextInt();
+		for (int i = 1; i <= hang; i++) {
+			for (int j = 1; j <= cot; j++) {
 				System.out.print(j + " x " + i + " = " + i * j + "\t");
 			}
 			System.out.println();
