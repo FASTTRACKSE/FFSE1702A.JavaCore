7615bff916a715ff4c43b487db11e82a1e3a3c4f
Longtlp <32054942+Longtlp@users.noreply.github.com>
2018-01-31 09:22:15 +0700

    long

--- FFSE1702021/Longtlp/src/Longtlp.java
+++ FFSE1702021/Longtlp/src/Longtlp.java
@@ -1,6 +1,11 @@
 
 public class Longtlp {
 public static void main(String[] args) {
-	System.out.println("Hello world, My name is Hieu ngu ml");
+	int a=5;
+	int b=7;
+	int c=a+b;
+	System.out.println(c);
+	
+	
 }
 }
