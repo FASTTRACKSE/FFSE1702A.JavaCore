81344e78b70eeec1967f65cadd8a3c05120d6556
ffse1702005 <ffse1702005@st.fasttrack.edu.vn>
2018-01-31 20:46:37 +0700

    bt

--- ffse1702005/java/src/text/text.java
+++ ffse1702005/java/src/text/text.java
@@ -5,10 +5,10 @@ public class text
 	public static void main(String[] args)
 	{
 	Scanner myInput = new Scanner(System.in);
-	//System.out.print("Nhập vào 1 số :");
-	//int number = myInput.nextInt();
-	for(int i=2; i<=9; i++) {
-		for(int j=1; j<=10; j++) {
+	System.out.print("Nhập vào 1 số :");
+	int number = myInput.nextInt();
+	for(int i=1; i<=10; i++) {
+		for(int j=1; j<=number; j++) {
 			System.out.printf("%5d x %2d = %2d", j , i , i*j);
 		}
 		System.out.println("");
