e6c6f52dcd91981ea6dd7c061ecb7533601f38e1
junbjn98 <23205674+junbjn98@users.noreply.github.com>
2018-01-31 11:18:37 +0700

    doc so (update)
    đọc số 2 chữ số

--- FFSE1703006/HelloWorld/src/first_project/doc_so.java
+++ FFSE1703006/HelloWorld/src/first_project/doc_so.java
@@ -12,12 +12,16 @@ public class doc_so {
 		num = scanIn.nextInt();
 		num1 = num / 10;
 		num2 = num % 10;
-		String read[] = { "mươi", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
-		if (num2 == 0) {
+		String read[] = { "mươi", "mốt", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+		if (num == 10) {
+			System.out.println(num + " => " + "mười ");
+		} else if (num1 == 1) {
+			read[1] = "một";
+			System.out.println(num + " => " + "mười " + read[num2]);
+		} else if (num2 == 0) {
 			System.out.println(num + " => " + read[num1] + " " + read[num2]);
 		} else {
 			System.out.println(num + " => " + read[num1] + " mươi " + read[num2]);
 		}
 	}
-
 }
