1a00f27e4b6bc988efa39b8f5ab44a9e9cea252c
ffse1702026 <32056534+ffse1702026@users.noreply.github.com>
2018-01-31 10:40:37 +0700

    lam lai

--- ffse1702026/bangcuuchuong/src/bangcuuchuong/bangcuuchuong.java
+++ ffse1702026/bangcuuchuong/src/bangcuuchuong/bangcuuchuong.java
@@ -13,13 +13,15 @@ public class bangcuuchuong {
 	        
 	        for (int i = 1; i <= 10; i++) { 
 	        	for (int j=1;j<=n;j++) {
-	            System.out.print(j + " x " + i + " = " + (i * j)+ "    "); 
+	        		if(j==1) {
+	        			System.out.printf("%2d x %2d = %2d",j,i,i*j); 
+	        		}
+	        		else {
+	        			System.out.printf("%12d x %2d = %2d",j,i,i*j);
+	        		}
 	        	 }
 	        	System.out.println(""); 
 	        } 
-	        
-	       
-	        
 
 	    } 
 
