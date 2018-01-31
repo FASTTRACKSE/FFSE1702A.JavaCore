cccff86722cc61aeb992ddcf1ba825aa7c0447d2
nguyencongquanghtml <32055155+nguyencongquanghtml@users.noreply.github.com>
2018-01-31 10:03:00 +0700

    QQQ

--- ffse1702024/cuuchuong/src/cuuchuong/cuuchuong.java
+++ ffse1702024/cuuchuong/src/cuuchuong/cuuchuong.java
@@ -4,22 +4,15 @@ public class cuuchuong {
 	
 	
 
-	    public void bang() { 
+	    public static void main(String args[]) { 
 	        int n; 
 	        Scanner s = new Scanner(System.in); 
-	        System.out.print("nhap so : "); 
+	        System.out.print("nhap 1 so : "); 
 	        n = s.nextInt(); 
-	        System.out.println("bang cuu chuong cua " + n +  " la"); 
+	        System.out.println("Bang cuu chuong " + n +  " la"); 
 	        for (int j = 0; j < 10; j++) { 
-	            System.out.println(n + "x" + j + " = " + (j * n)); 
+	            System.out.println(n + " x " + j + " = " + (j * n)); 
 	        } 
-	        System.out.println(""); 
-
-	    } 
-
-	    public static void main(String args[]) { 
-	    	cuuchuong n = new cuuchuong(); 
-	        n.bang(); 
-
+	        System.out.println("");
 	    } 
 	} 
