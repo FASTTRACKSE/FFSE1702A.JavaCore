3973973814ef21794ab931e43ec576b2a00bb32b
lehuuhoainam <ffse1703007@st.fasttrack.edu.vn>
2018-01-31 10:35:57 +0700

    bang cuu chuong
    bang cuu chuong

--- FFSE1703007/Hello_World/src/Hello_World/Hellworld.java
+++ FFSE1703007/Hello_World/src/Hello_World/Hellworld.java
@@ -3,8 +3,10 @@ package Hello_World;
 public class Hellworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		System.out.println(c);
 	}
 
 }
