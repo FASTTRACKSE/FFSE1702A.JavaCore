f3a76fe77c0b6f333760889d7a02cf5af2071d19
tranthitam <tranthitam@github.com>
2018-01-31 23:13:49 +0700

    ffse1702029

---
+++ ffse1702029/HelloWorld2/src/DocSo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DocSo {
+	public static void main(String[] args) {
+
+		Scanner myInput = new Scanner(System.in);
+		System.out.print("nhập vào 1 số : ");
+		int so = myInput.nextInt();
+		
+	}
+}
