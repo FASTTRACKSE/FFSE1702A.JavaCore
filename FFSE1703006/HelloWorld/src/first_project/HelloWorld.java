f346c3f19cee1d2dbac4cfbf927ccd38237b4b85
junbjn98 <23205674+junbjn98@users.noreply.github.com>
2018-01-31 09:21:06 +0700

    test

--- FFSE1703006/HelloWorld/src/first_project/HelloWorld.java
+++ FFSE1703006/HelloWorld/src/first_project/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World! 2222222");
+		int a = 2, b = 3, c = 4;
+		System.out.println("Tong " + (a + b + c));
 	}
 }
