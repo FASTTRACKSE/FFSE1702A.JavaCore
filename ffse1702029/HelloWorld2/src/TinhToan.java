837fa1ff1bb47e247ae48ddfa69fa5245455b501
tranthitam <tranthitam@github.com>
2018-02-02 11:28:47 +0700

    commit code TinhToan

--- ffse1702029/HelloWorld2/src/TinhToan.java
+++ ffse1702029/HelloWorld2/src/TinhToan.java
@@ -13,35 +13,35 @@ public class TinhToan {
 		System.out.println("+-------------------+");
 		System.out.println(">> Chọn chức năng? ");
 		System.out.println("Xin mời nhập số ");
-		
-		int n= TinhToan.nextInt();
-		
-		if(n<1||n>3){
+
+		int n = TinhToan.nextInt();
+
+		if (n < 1 || n > 3) {
 			System.out.println("Nhập số từ 1 đến 3");
 		}
-		if(n==1) {
+		if (n == 1) {
 			@SuppressWarnings("resource")
-			Scanner so =new Scanner(System.in);
+			Scanner so = new Scanner(System.in);
 			System.out.println("Mời nhập số thứ 1");
-			int a= so.nextInt();
+			int a = so.nextInt();
 			@SuppressWarnings("resource")
-			Scanner so2 =new Scanner(System.in);
+			Scanner so2 = new Scanner(System.in);
 			System.out.println("Mời nhập số thứ 2");
-			int b= so2.nextInt();
-			System.out.println(a+b);
+			int b = so2.nextInt();
+			System.out.println(a + b);
 		}
-		if(n==2) {
+		if (n == 2) {
 			@SuppressWarnings("resource")
-			Scanner so =new Scanner(System.in);
+			Scanner so = new Scanner(System.in);
 			System.out.println("Mời nhập số thứ 1");
-			int a= so.nextInt();
+			int a = so.nextInt();
 			@SuppressWarnings("resource")
-			Scanner so2 =new Scanner(System.in);
+			Scanner so2 = new Scanner(System.in);
 			System.out.println("Mời nhập số thứ 2");
-			int b= so2.nextInt();
-			System.out.println(a-b);
+			int b = so2.nextInt();
+			System.out.println(a - b);
 		}
-		
+
 	}
 
 }
