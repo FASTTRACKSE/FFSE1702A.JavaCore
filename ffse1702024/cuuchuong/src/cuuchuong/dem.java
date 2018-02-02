dc4efc4bbc967a2ca5fc6b2bccd0966c267b1c4e
nguyencongquanghtml <32055155+nguyencongquanghtml@users.noreply.github.com>
2018-02-02 17:09:45 +0700

    QQQ

---
+++ ffse1702024/cuuchuong/src/cuuchuong/dem.java
@@ -0,0 +1,34 @@
+package cuuchuong;
+
+import java.util.Scanner;
+
+public class dem {
+	 public static void main(String args[]) {
+		 String[] aryString = new String[10] ;
+		 aryString[0] ="muoi";
+		 aryString[1] ="một";
+		 aryString[2] ="hai";
+		 aryString[3] ="ba";
+		 aryString[4] ="bốn";
+		 aryString[5] ="năm";
+		 aryString[6] ="sáu";
+		 aryString[7] ="bảy";
+		 aryString[8] ="tám";
+		 aryString[9] ="chín";
+		 
+		
+		 
+		 Scanner bien = new Scanner(System.in);
+		 System.out.println("Nhap vao 1 so : ");
+		 int n = bien.nextInt();
+		 int x = n/10;
+		 int y = n-x*10;
+		 System.out.println(x + y);
+
+		 char[] kyTu = new char[] {'a', 'b', 'c', 'd', 'e'};
+
+		 }
+		 
+		 }
+		 
+				
