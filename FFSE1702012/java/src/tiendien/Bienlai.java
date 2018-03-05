09878c164aa1d3c07ef68bdaa0ebe2af9d02c257
FFSE1702012 <FFSE1702012@github.com>
2018-03-05 08:59:16 +0700

    assignment 8

---
+++ FFSE1702012/java/src/tiendien/Bienlai.java
@@ -0,0 +1,49 @@
+package tiendien;
+    import java.util.Scanner;
+   import java.util.ArrayList;
+public class Bienlai {
+    Khachhang kh = new Khachhang();
+    ArrayList<Khachhang> arrkh = new ArrayList();
+    public static Scanner myinput = new Scanner(System.in); 
+   public int chisocu;
+    public int chisomoi;
+    private Khachhang  Khachhang;
+    private double sotienphaitra;
+    public Bienlai() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    public Bienlai(tiendien.Khachhang kh, int chisocu, int chisomoi,
+            tiendien.Khachhang khachhang, double sotienphaitra) {
+        super();
+        this.kh = kh;
+        this.chisocu = chisocu;
+        this.chisomoi = chisomoi;
+        Khachhang = khachhang;
+        this.sotienphaitra = sotienphaitra;
+    }
+    public void nhap() {
+            kh.nhap();
+            System.out.println("NHap thong tin bien lai khach hang ");
+            System.out.println("chi so cong to cu");
+            int old = myinput.nextInt();
+            if(old<=0) {System.out.println("Yêu cầu nhập lai");}
+            else chisocu = old;
+            System.out.println("chi so cong to mới");
+            int new1 = myinput.nextInt();
+            if(new1<=0) {System.out.println("Yêu cầu nhập lai");}
+            else chisomoi = new1;
+            if(chisomoi<chisocu) {
+                System.out.println("Yêu cầu nhập lai");
+            }
+            else
+                sotienphaitra = (chisomoi - chisocu)*750;
+        }
+    public void xuat() {
+        kh.xuat();
+        System.out.println("Chỉ số cũ: " + chisocu);
+        System.out.println("Chỉ số mới: " + chisomoi);
+        System.out.println("Số tiền phải trả: " + sotienphaitra);
+    }
+    }
+
---
+++ FFSE1702012/java/src/tiendien/Demo.java
@@ -0,0 +1,17 @@
+package tiendien;
+
+import java.util.Comparator;
+
+import student.SinhVien;
+
+public class Demo {
+    public static void main(String args[]) {
+        String[] sv1= {"Anh", "Ban", "Chi", "Danh"} ;
+        
+    }
+
+    public int compare(sv1 a) {
+            return sv1.name.compareTo(sv2.name);
+        }
+
+}
