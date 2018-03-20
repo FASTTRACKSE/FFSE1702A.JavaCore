d130ba189e15c65eb4c7a4b4ace5b49590a938e1
Longtlp <32054942+Longtlp@users.noreply.github.com>
2018-03-20 22:00:05 +0700

    long

--- FFSE1702021/TinhTienDien/src/tiendien/TienDien.java
+++ FFSE1702021/TinhTienDien/src/tiendien/TienDien.java
@@ -1,13 +1,13 @@
-package src.tiendien;
+package tiendien;
 
 import java.util.Scanner;
 
-public class tiendien {
+public class TienDien {
 	public static void main(String[] args) {
 		String kt ="yes";
 		while( kt == "yes") {
 			try {
-		System.out.print("Nhập số chữ điện\n");
+		System.out.print("nhập số chử điện");
 		Scanner myInput= new Scanner(System.in);
 		int chudien =myInput.nextInt();
 		int a = 50*1549;
@@ -18,33 +18,33 @@ public class tiendien {
 		
 		if(chudien <= 50) {
 			double thue = (chudien*1549)/10;
-			System.out.print("Ti�?n điện :"+ ((chudien*1549)+ thue));
+			System.out.print("Tiền Điện :"+ ((chudien*1549)+ thue));
 		}
 		else if(chudien <= 100 ) {
 			double thue = (a + ((chudien-50)*1600))/10;
-			System.out.print("Ti�?n điện :"+ ((a + ((chudien-50)*1600))+ thue ));
+			System.out.print("Tiền Điện :"+ ((a + ((chudien-50)*1600))+ thue ));
 		}
 		else if(chudien <= 200 ) {
 			double thue = (a + b + ((chudien-100)*1858))/10;
-			System.out.print("Ti�?n điện :"+ ((a + b + ((chudien-100)*1858))+thue));
+			System.out.print("Tiền Điện :"+ ((a + b + ((chudien-100)*1858))+thue));
 		}
 		else if(chudien <= 300 ) {
 			double thue = (a + b + c + ((chudien-200)*2340))/10;
-			System.out.print("Ti�?n điện :"+ ((a + b + c + ((chudien-200)*2340))+ thue));
+			System.out.print("Tiền Điện :"+ ((a + b + c + ((chudien-200)*2340))+ thue));
 		}
 		else if(chudien <= 400 ) {
 			double thue = (a + b + c + d + ((chudien-200)*2615))/10;
-			System.out.print("Ti�?n điện :"+ ((a + b + c + d + ((chudien-300)*2615))+ thue));
+			System.out.print("Tiền Điện :"+ ((a + b + c + d + ((chudien-300)*2615))+ thue));
 		}
 		else if(chudien > 400) {
 			double thue = (a + b + c + d + e + ((chudien-200)*2701))/10;
-			System.out.print("Ti�?n điện :"+ ((a + b + c + d + e + (chudien*2701))+ thue));
+			System.out.print("Tiền Điện :"+ ((a + b + c + d + e + (chudien*2701))+ thue));
 		 }
 			} catch(Exception ex) {
-				System.out.print("\n vui lòng nhập số");
+				System.out.print("\n vui long nhập số");
 			}
 		Scanner myInput2 = new Scanner(System.in);
-		System.out.println("\n Muốn dừng nhấn N không muốn dừng thì tùy thích mà nhấn nhé  ");
+		System.out.println("\n muốn dừng chương trinh nhấn N típ tục nhấn y  ");
 		kt = myInput2.nextLine();
 		if (kt.equalsIgnoreCase("N") || kt.equalsIgnoreCase("No")) {
 			break;
@@ -56,3 +56,4 @@ public class tiendien {
 
 
 }
+
