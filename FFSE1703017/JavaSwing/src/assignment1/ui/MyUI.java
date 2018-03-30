54ea14960b7e15f71adbfb542e7c32d76598bdc8
vngoc <35908057+vngoc@users.noreply.github.com>
2018-03-30 08:56:28 +0700

    JavaDesktop : Update assignment 1
    Viết theo coding conventions.

--- FFSE1703017/JavaSwing/src/assignment1/ui/MyUI.java
+++ FFSE1703017/JavaSwing/src/assignment1/ui/MyUI.java
@@ -3,11 +3,14 @@ package assignment1.ui;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -21,9 +24,50 @@ public class MyUI extends JFrame {
 	JButton btnThoat = new JButton("Thoát");
 	JButton btnHelp = new JButton("Help");
 	
+	ActionListener eventGiai = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String hsA = txtHeSoA.getText();
+			String hsB = txtHeSoB.getText();
+			boolean isA = chkDouble(hsA);
+			boolean isB = chkDouble(hsB);
+			if (isA && isB) {
+				Double a = Double.parseDouble(hsA);
+				Double b = Double.parseDouble(hsB);
+				if (a == 0) {
+					String s = (b == 0) ? "Phương trình vô số nghiệm" : "Phương trình vô nghiệm";
+					txtKetQua.setText(s);
+				} else {
+					txtKetQua.setText("x = " + (-b/a));
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Hệ số không được để trống và phải nhập đúng định dạng", "Thông báo", JOptionPane.WARNING_MESSAGE);
+			}
+		}
+	};
+	
+	ActionListener eventThoat = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	};
+	
+	ActionListener eventHelp = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String msg = "Phương trình bậc nhất có dạng ax + b = 0, nhập vào hệ số a và b sau đó nhấn Giải để xem kết quả.";
+			JOptionPane.showMessageDialog(null, msg, "Hướng dẫn", JOptionPane.INFORMATION_MESSAGE);
+		}
+	};
+	
 	public MyUI (String title) {
 		super(title);
 		addControls();
+		addEvents();
 	}
 	
 	public void addControls() {
@@ -70,10 +114,25 @@ public class MyUI extends JFrame {
 		con.add(pnMain);
 	}
 	
+	public void addEvents() {
+		btnGiai.addActionListener(eventGiai);
+		btnThoat.addActionListener(eventThoat);
+		btnHelp.addActionListener(eventHelp);
+	}
+	
 	public void showWindow() {
 		this.setSize(400, 250);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+	}
+	
+	static boolean chkDouble(String s) {
+		try {
+			Double.parseDouble(s.trim());
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
