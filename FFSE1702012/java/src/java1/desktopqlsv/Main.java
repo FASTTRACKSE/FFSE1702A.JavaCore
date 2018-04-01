5d06cb6ace98ed73ae8472262e3ea815fb5b1826
FFSE1702012 <FFSE1702012@github.com>
2018-04-01 16:13:26 +0700

    assignment

---
+++ FFSE1702012/java/src/java1/desktopqlsv/Main.java
@@ -0,0 +1,8 @@
+package java1.desktopqlsv;
+
+public class Main {
+	public static void main(String args[]) {
+		QuanLySinhVien qlsv = new QuanLySinhVien("Chương trình quản lý sinh viên");
+		qlsv.showWindow();
+	}
+}
