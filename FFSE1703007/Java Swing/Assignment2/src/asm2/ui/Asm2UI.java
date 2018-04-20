930c3afb5c7b3ba150a3c84fdf3d30282a107c73
lehuuhoainam <ffse1703007@st.fasttrack.edu.vn>
2018-04-20 21:11:55 +0700

    up Project LP4
    up Project LP4

--- FFSE1703007/Java Swing/Assignment2/src/asm2/ui/Asm2UI.java
+++ FFSE1703007/Java Swing/Assignment2/src/asm2/ui/Asm2UI.java
@@ -1,32 +1,56 @@
 package asm2.ui;
 
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Vector;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+
+import asm2.model.MySQL;
+import asm2.model.Sinhvien;
+
 public class Asm2UI extends JFrame {
 	JTextField txtID, txtName, txtAge;
-	JButton btnAdd, btnEdit, btnDel, btnExit;
+	JButton btnAdd, btnEdit, btnDel, btnExit, btnWrite, btnRead;
 	JTable table;
+	JComboBox cb;
+	ArrayList<Sinhvien> list = new ArrayList<Sinhvien>();
+	String[] courseArray = { "FFSE1701", "FFSE1702", "FFSE1703", "FFSE1704" };
+	Connection conn = MySQL.getConnect("localhost", "java", "java", "12345");
 
 	public Asm2UI(String title) {
 		super(title);
 		addControls();
 		addEvents();
+		changeCourse();
 	}
 
 	public void addControls() {
@@ -40,20 +64,30 @@ public class Asm2UI extends JFrame {
 		lblTitle.setFont(fontTitle);
 		pnTitle.add(lblTitle);
 
+		JPanel pnComboBox = new JPanel();
+		JLabel lblComboBox = new JLabel("Chọn lớp");
+		String[] course = { "FFSE1701", "FFSE1702", "FFSE1703", "FFSE1704" };
+		cb = new JComboBox<>(course);
+		pnComboBox.add(lblComboBox);
+		pnComboBox.add(cb);
+
 		JPanel pnInput1 = new JPanel();
 		JLabel lblTitle1 = new JLabel("Mã sinh viên");
+		lblTitle1.setPreferredSize(new Dimension(80, 10));
 		txtID = new JTextField(15);
 		pnInput1.add(lblTitle1);
 		pnInput1.add(txtID);
 
 		JPanel pnInput2 = new JPanel();
 		JLabel lblTitle2 = new JLabel("Tên sinh viên");
+		lblTitle2.setPreferredSize(new Dimension(80, 10));
 		txtName = new JTextField(15);
 		pnInput2.add(lblTitle2);
 		pnInput2.add(txtName);
 
 		JPanel pnInput3 = new JPanel();
 		JLabel lblTitle3 = new JLabel("Tuổi");
+		lblTitle3.setPreferredSize(new Dimension(80, 10));
 		txtAge = new JTextField(15);
 		pnInput3.add(lblTitle3);
 		pnInput3.add(txtAge);
@@ -62,18 +96,24 @@ public class Asm2UI extends JFrame {
 		btnAdd = new JButton("Thêm");
 		btnEdit = new JButton("Sửa");
 		btnDel = new JButton("Xóa");
+		btnWrite = new JButton("Lưu File");
+		btnRead = new JButton("Đọc File");
 		btnExit = new JButton("Thoát");
 		pnAction.add(btnAdd);
 		pnAction.add(btnEdit);
 		pnAction.add(btnDel);
+		pnAction.add(btnWrite);
+		pnAction.add(btnRead);
 		pnAction.add(btnExit);
 
 		JScrollPane scrollPane = new JScrollPane();
 		table = new JTable();
-		table.setModel(new DefaultTableModel(new Object[][] {}, new String[] { "Mã", "Tên", "Tuổi" }));
+		DefaultTableModel model = new DefaultTableModel(new String[] { "Mã", "Tên", "Tuổi" }, 0);
+		table.setModel(model);
 		scrollPane.setViewportView(table);
 
 		pnMain.add(pnTitle);
+		pnMain.add(pnComboBox);
 		pnMain.add(pnInput1);
 		pnMain.add(pnInput2);
 		pnMain.add(pnInput3);
@@ -95,9 +135,23 @@ public class Asm2UI extends JFrame {
 	public void add() {
 		String iD = txtID.getText();
 		String name = txtName.getText();
-		String age = txtAge.getText();
+		int age = Integer.parseInt(txtAge.getText());
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
-		model.addRow(new Object[] { iD, name, age });
+		Object[] data = { iD, name, age };
+		model.addRow(data);
+		int indexSelectedCourse = cb.getSelectedIndex();
+
+		try {
+			String sql = "insert into students values (" + "'" + iD + "'" + "," + "'" + name + "'" + "," + "'" + age
+					+ "'" + "," + "'" + courseArray[indexSelectedCourse] + "'" + ")";
+			Statement statement = (Statement) conn.createStatement();
+			int x = statement.executeUpdate(sql);
+			if (x > 0) {
+				JOptionPane.showMessageDialog(null, "Lưu OK");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
 		txtID.setText("");
 		txtName.setText("");
@@ -118,7 +172,7 @@ public class Asm2UI extends JFrame {
 		int modelRow = table.convertRowIndexToModel(row);
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
 		model.removeRow(modelRow);
-		
+
 		txtID.setText("");
 		txtName.setText("");
 		txtAge.setText("");
@@ -131,16 +185,16 @@ public class Asm2UI extends JFrame {
 	};
 
 	public void getRow() {
-		JTextField jTextField[] = new JTextField[]{txtID, txtName, txtAge};
+		JTextField[] jTextField = { txtID, txtName, txtAge };
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
 		int numberOfColumn = table.getColumnCount();
 		int indexOfSelectedRow = table.getSelectedRow();
 		for (int i = 0; i < numberOfColumn; i++) {
-			String value = (model.getValueAt(indexOfSelectedRow, i)).toString();
+			String value = (String) (model.getValueAt(indexOfSelectedRow, i));
 			jTextField[i].setText(value);
 		}
 	}
-	
+
 	ActionListener eventEdit = new ActionListener() {
 
 		@Override
@@ -149,23 +203,46 @@ public class Asm2UI extends JFrame {
 			edit();
 		}
 	};
-	
+
 	public void edit() {
-		JTextField jTextField[] = new JTextField[]{txtID, txtName, txtAge};
+
+		String iD = txtID.getText();
+		String name = txtName.getText();
+		int age = Integer.parseInt(txtAge.getText());
+
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
-		int numberOfColumn = table.getColumnCount();
 		int indexOfSelectedRow = table.getSelectedRow();
-		for (int i = 0; i < numberOfColumn; i++) {
-			String value = jTextField[i].getText();
-			model.setValueAt(value, indexOfSelectedRow, i);
+		String oldID = (String) model.getValueAt(indexOfSelectedRow, 0);
+		model.setValueAt(iD, indexOfSelectedRow, 0);
+		model.setValueAt(name, indexOfSelectedRow, 1);
+		model.setValueAt(age, indexOfSelectedRow, 2);
+
+		try {
+			String sql = "update students set id=?, name=?, age=? where id=?";
+
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			stm.setString(1, iD);
+			stm.setString(2, name);
+			stm.setInt(3, age);
+			stm.setString(4, oldID);
+			int x = stm.executeUpdate();
+			if (x > 0) {
+				JOptionPane.showMessageDialog(null, "Cập nhật OK");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
+
 	}
 
 	public void addEvents() {
 		btnAdd.addActionListener(eventAdd);
 		btnDel.addActionListener(eventDel);
 		btnEdit.addActionListener(eventEdit);
+		// btnWrite.addActionListener(eventAddToMySQL);
+		// btnRead.addActionListener(eventReadFromMySQL);
 		table.addMouseListener(eventGetRow);
+		cb.addActionListener(eventChangeCourse);
 		btnExit.addActionListener(new ActionListener() {
 
 			@Override
@@ -182,4 +259,31 @@ public class Asm2UI extends JFrame {
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
+
+	ActionListener eventChangeCourse = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			changeCourse();
+		}
+	};
+
+	public void changeCourse() {
+		int indexSelectedCourse = cb.getSelectedIndex();
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		model.setRowCount(0);
+		try {
+			Statement statement = (Statement) conn.createStatement();
+			ResultSet result = statement.executeQuery(
+					"select id,name,age from students where course =" + "'" + courseArray[indexSelectedCourse] + "'");
+			while (result.next()) {
+				Object[] data = { result.getString(1), result.getString(2), result.getString(3) };
+				model.addRow(data);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
---
+++ FFSE1703007/Project/Electricity Invoice Management/src/model/MySQL.java
@@ -0,0 +1,334 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.Properties;
+
+import org.gjt.mm.mysql.Driver;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+
+public class MySQL {
+	static Connection conn = getConnect("localhost", "java", "java", "12345");
+
+	public static Connection getConnect(String strServer, String strDatabase, String strUser, String strPwd) {
+		Connection conn = null;
+		String strConnect = "jdbc:mysql://" + strServer + "/" + strDatabase
+				+ "?useUnicode=true&characterEncoding=utf-8";
+		Properties pro = new Properties();
+		pro.put("user", strUser);
+		pro.put("password", strPwd);
+		try {
+			com.mysql.jdbc.Driver driver = new Driver();
+			conn = (Connection) driver.connect(strConnect, pro);
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static boolean checkConnection() {
+		if (conn != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean checkLogin(String username, String password) {
+		try {
+			String sql = "select * from user where username = ? and password = ?";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			stm.setString(1, username);
+			stm.setString(2, password);
+			ResultSet result = stm.executeQuery();
+			if (!result.next()) {
+				return false;
+			} else {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static ResultSet getCounty() {
+		try {
+			String sql = "select name from county";
+			Statement statement = (Statement) conn.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static ResultSet getWard(int countyID) {
+		try {
+			String sql = "select * from ward where county = ?";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			stm.setInt(1, countyID);
+			ResultSet result = stm.executeQuery();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static boolean addCustomer(String fullname, String address, int countyID, int wardID, String phone,
+			String email, String meterID) {
+		try {
+			String sql = "insert into customer ( fullname, address, countyID, wardID, phone, email, meterID ) values ( ?, ?, ?, ?, ?, ?, ? )";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+
+			stm.setString(1, fullname);
+			stm.setString(2, address);
+			stm.setInt(3, countyID);
+			stm.setInt(4, wardID);
+			stm.setString(5, phone);
+			stm.setString(6, email);
+			stm.setString(7, meterID);
+			int x = stm.executeUpdate();
+			if (x > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static ResultSet getCustomerList(String countyName, String wardName) {
+		try {
+			String sql = "SELECT customer.id, customer.fullname, customer.address, county.name, ward.name, customer.phone, customer.email, customer.meterID,customer.countyID,customer.wardID FROM ((customer INNER JOIN county on customer.countyID = county.id)\r\n"
+					+ "               INNER JOIN ward ON customer.wardID = ward.id) where county.name like ? and ward.name like ?";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			stm.setString(1, "%"+countyName+"%");
+			stm.setString(2, "%"+wardName+"%");
+			ResultSet result = stm.executeQuery();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static boolean editCustomer(String fullname, String address, int countyID, int wardID, String phone,
+			String email, String meterID, String customerID) {
+		try {
+			String sql = "update customer set fullname = ?, address = ?, countyID = ?, wardID = ? , phone = ?, email = ?, meterID = ? where id=?";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+
+			stm.setString(1, fullname);
+			stm.setString(2, address);
+			stm.setInt(3, countyID);
+			stm.setInt(4, wardID);
+			stm.setString(5, phone);
+			stm.setString(6, email);
+			stm.setString(7, meterID);
+			stm.setString(8, customerID);
+			int x = stm.executeUpdate();
+			if (x > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static boolean delCustomer(String customerID) {
+		try {
+			String sql = "delete from customer where id=?";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+
+			stm.setString(1, customerID);
+
+			int x = stm.executeUpdate();
+			if (x > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static ResultSet getInvoice(String meterID) {
+		try {
+			String sql = "SELECT * from invoice where meterID = ?";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			stm.setString(1, meterID);
+			ResultSet result = stm.executeQuery();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static boolean addInvoice(String meterID, String dateAdded, Date cycle, int meterIndex, int amount) {
+		try {
+			String sql = "insert into invoice ( meterID, dateAdded, cycle, meterIndex, amount) values ( ?, ?, ?, ?, ? )";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+
+			java.sql.Date sqlDate = new java.sql.Date(cycle.getTime());
+
+			stm.setString(1, meterID);
+			stm.setString(2, dateAdded);
+			stm.setDate(3, sqlDate);
+			stm.setInt(4, meterIndex);
+			stm.setInt(5, amount);
+			int x = stm.executeUpdate();
+			if (x > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static ResultSet getLastestInvoice(String meterID) {
+		try {
+			String sql = "SELECT id,meterID, meterIndex from invoice where meterID = ? ORDER BY id DESC LIMIT 1";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			stm.setString(1, meterID);
+			ResultSet result = stm.executeQuery();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static boolean editInvoice(Date cycle, String meterIndex, int amount, int invoiceID) {
+		try {
+			String sql = "update invoice set cycle = ?, meterIndex = ?, amount = ? where id=?";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+
+			java.sql.Date sqlDate = new java.sql.Date(cycle.getTime());
+
+			stm.setDate(1, sqlDate);
+			stm.setString(2, meterIndex);
+			stm.setInt(3, amount);
+			stm.setInt(4, invoiceID);
+
+			int x = stm.executeUpdate();
+			if (x > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static ResultSet getLastestInvoiceForEdit(String meterID, int InvoiceID) {
+		try {
+			String sql = "SELECT * from invoice where meterID = ? AND (id BETWEEN 1 AND ?)  ORDER BY id DESC LIMIT 1";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			stm.setString(1, meterID);
+			stm.setInt(2, InvoiceID - 1);
+			ResultSet result = stm.executeQuery();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static boolean delInvoice(String invoiceID) {
+		try {
+			String sql = "delete from invoice where id=?";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+
+			stm.setString(1, invoiceID);
+
+			int x = stm.executeUpdate();
+			if (x > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static ResultSet getDataBySearch1(String customerName, String countyName, String wardName, String cycle) {
+		try {
+			String sql = "SELECT customer.id, customer.meterID,customer.fullname, customer.address, county.name, ward.name, customer.phone, customer.email, invoice.cycle,invoice.amount,customer.countyID,customer.wardID FROM (((customer INNER JOIN county on customer.countyID = county.id)\r\n"
+					+ "					              INNER JOIN ward ON customer.wardID = ward.id) INNER JOIN invoice ON customer.meterID = invoice.meterID) where customer.fullname like ? and county.name like ? and ward.name like ? and invoice.cycle like ?";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			stm.setString(1, "%" + customerName + "%");
+			stm.setString(2, "%" + countyName + "%");
+			stm.setString(3, "%" + wardName + "%");
+			stm.setString(4, "%" + cycle + "%");
+			ResultSet result = stm.executeQuery();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static ResultSet getDataBySearch2(String customerName, String countyName, String wardName, Date cycleStart,Date cycleEnd) {
+		try {
+			String sql = "SELECT customer.id, customer.meterID,customer.fullname, customer.address, county.name, ward.name, customer.phone, customer.email, invoice.cycle,invoice.amount,customer.countyID,customer.wardID FROM (((customer INNER JOIN county on customer.countyID = county.id)\r\n"
+					+ "					              INNER JOIN ward ON customer.wardID = ward.id) INNER JOIN invoice ON customer.meterID = invoice.meterID) where customer.fullname like ? and county.name like ? and ward.name like ? and (invoice.cycle between ? and ?)";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			
+			java.sql.Date sqlCycleStart = new java.sql.Date(cycleStart.getTime());
+			java.sql.Date sqlCycleEnd = new java.sql.Date(cycleEnd.getTime());
+			
+			stm.setString(1, "%" + customerName + "%");
+			stm.setString(2, "%" + countyName + "%");
+			stm.setString(3, "%" + wardName + "%");
+			stm.setDate(4, sqlCycleStart);
+			stm.setDate(5, sqlCycleEnd);
+			ResultSet result = stm.executeQuery();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static ResultSet getDataBySearch3(String customerName, String countyName, String wardName, Date cycle) {
+		try {
+			String sql = "SELECT customer.id, customer.meterID,customer.fullname, customer.address, county.name, ward.name, customer.phone, customer.email, invoice.cycle,invoice.amount,customer.countyID,customer.wardID FROM (((customer INNER JOIN county on customer.countyID = county.id)\r\n"
+					+ "					              INNER JOIN ward ON customer.wardID = ward.id) INNER JOIN invoice ON customer.meterID = invoice.meterID) where customer.fullname like ? and county.name like ? and ward.name like ? and invoice.cycle = ? ";
+			PreparedStatement stm = (PreparedStatement) conn.prepareStatement(sql);
+			
+			java.sql.Date sqlCycle = new java.sql.Date(cycle.getTime());
+			
+			stm.setString(1, "%" + customerName + "%");
+			stm.setString(2, "%" + countyName + "%");
+			stm.setString(3, "%" + wardName + "%");
+			stm.setDate(4, sqlCycle);
+			ResultSet result = stm.executeQuery();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
