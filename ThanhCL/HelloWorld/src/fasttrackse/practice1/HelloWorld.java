478dffd285f2a6db36c757e7b0d48f4511db1fd4
ThanhCL <thanhcl@gmail.com>
2021-03-05 11:57:12 +0700

    backup

---
+++ ThanhCL/HelloWorld/src/fasttrackse/practice1/HelloWorld.java
@@ -0,0 +1,10 @@
+package fasttrackse.practice1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World from THANHCL");
+		System.out.println("This is my 1st Java Program");
+	}
+
+}
